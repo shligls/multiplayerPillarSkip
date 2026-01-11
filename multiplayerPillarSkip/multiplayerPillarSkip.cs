998d9d172dc675dbf3504abb43b526df27fd6879
shligls <parkerwilliam0731@gmail.com>
2026-01-11 17:55:54 -0500

    Added gutted files from the boilerplate.

---
+++ multiplayerPillarSkip/multiplayerPillarSkip.cs
@@ -0,0 +1,67 @@
+using BepInEx;
+using R2API;
+using RoR2;
+using RoR2BepInExPack.GameAssetPaths;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace ExamplePlugin
+{
+    // This attribute specifies that we have a dependency on a given BepInEx Plugin,
+    // We need the R2API ItemAPI dependency because we are using for adding our item to the game.
+    // You don't need this if you're not using R2API in your plugin,
+    // it's just to tell BepInEx to initialize R2API before this plugin so it's safe to use R2API.
+    [BepInDependency(ItemAPI.PluginGUID)]
+
+    // This attribute is required, and lists metadata for your plugin.
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+
+    // This is the main declaration of our plugin class.
+    // BepInEx searches for all classes inheriting from BaseUnityPlugin to initialize on startup.
+    // BaseUnityPlugin itself inherits from MonoBehaviour,
+    // so you can use this as a reference for what you can declare and use in your plugin class
+    // More information in the Unity Docs: https://docs.unity3d.com/ScriptReference/MonoBehaviour.html
+    public class multiplayerPillarSkip : BaseUnityPlugin
+    {
+        
+        // The Plugin GUID should be a unique ID for this plugin,
+        // which is human readable (as it is used in places like the config).
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "shligls";
+        public const string PluginName = "Multiplayer Pillar Skip";
+        public const string PluginVersion = "1.0.0";
+
+        // The Awake() method is run at the very start when the game is initialized.
+        public void Awake()
+        {
+            // Init our logging class so that we can properly log for debugging
+            Log.Init(Logger);
+
+
+
+            // But now we have defined an item, but it doesn't do anything yet. So we'll need to define that ourselves.
+            GlobalEventManager.onCharacterDeathGlobal += GlobalEventManager_onCharacterDeathGlobal;
+        }
+
+        private void GlobalEventManager_onCharacterDeathGlobal(DamageReport report)
+        {
+
+        }
+
+        // The Update() method is run on every frame of the game.
+        private void Update()
+        {
+            // This if statement checks if the player has currently pressed F2.
+            if (Input.GetKeyDown(KeyCode.F2))
+            {
+                // Get the player body to use a position:
+                var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
+
+                // And then drop our defined item in front of the player.
+
+                Log.Info($"Player pressed F2. Spawning our custom item at coordinates {transform.position}");
+                PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(myItemDef.itemIndex), transform.position, transform.forward * 20f);
+            }
+        }
+    }
+}
